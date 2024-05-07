aaf7dec98b0a668e2fbe7c4149d31dc60beaa3f9
buyab_h9pfnco <buyab_h9pfnco@MOSHOPC>
2024-05-07 16:12:37 +0300

    JourneyIt.v3

---
+++ JourneyItMVC/Controllers/AddPostController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace JourneyItMVC.Controllers
+{
+    public class AddPostController : Controller
+    {
+        // GET: /AddPost/Create
+        public IActionResult Create()
+        {
+            // Populate countries dropdown
+            var countries = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "USA", Value = "USA" },
+                new SelectListItem { Text = "Canada", Value = "Canada" },
+                // Add more countries as needed
+            };
+
+            ViewBag.Countries = countries;
+
+            return View();
+        }
+
+        //// POST: /AddPost/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Create(AddPost post)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        // Save the blog post to the database
+        //        // Example: SaveToDatabase(post);
+
+        //        // Redirect to a success page or homepage
+        //        return RedirectToAction("Index", "Home");
+        //    }
+
+        //    // If ModelState is not valid, return back to the create form with errors
+        //    // Populate countries dropdown
+        //    var countries = new List<SelectListItem>
+        //    {
+        //        new SelectListItem { Text = "USA", Value = "USA" },
+        //        new SelectListItem { Text = "Canada", Value = "Canada" },
+        //        // Add more countries as needed
+        //    };
+
+        //    ViewBag.Countries = countries;
+
+        //    return View(post);
+        //}
+    }
+}
+
--- JourneyItMVC/Program.cs
+++ JourneyItMVC/Program.cs
@@ -1,7 +1,28 @@
+
+using JourneyItMVC.API;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+
+
+builder.Services.AddHttpClient("JourneyIt", c => c.BaseAddress = new System.Uri("https://localhost:7005"));
+builder.Services.AddScoped<JourneyItAPI>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.SlidingExpiration = true;
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                    options.LoginPath = "/Account/Login";
+                    options.LogoutPath = "/Account/Logout";
+                    options.AccessDeniedPath = "/Account/Denied";
+                });
 
 var app = builder.Build();
 
@@ -15,10 +36,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     name: "default",
